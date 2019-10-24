ab84e271e3df2cc47b72df4de7370928be09626a
madeleinesvensson <56533906+madeleinesvensson@users.noreply.github.com>
2019-10-24 09:58:07 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-label.switch > input[type="checkbox"]:checked ~ span:before {
-  transform: scale(0);
-  opacity: .7;
-}
- 
-label.switch > input[type="checkbox"]:checked ~ span:after {
-  transform: translate3d(100%, -50%, 0);
-}
